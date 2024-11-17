0992c3756cd19111dd25af7d5216d7bb58e61379
Franz Höpfinger <mail@franzhoepfinger.de>
2024-11-17 13:53:59 +0100

    Add URC handler for modem support
    - Implemented a new function to handle URC messages.
    - Added logging for received data in hex format.
    - Included conditional compilation based on modem config.

--- simple_cmux_client/components/simple_cmux_client/simple_cmux_client_main.cpp
+++ simple_cmux_client/components/simple_cmux_client/simple_cmux_client_main.cpp
@@ -116,6 +116,14 @@ private:
 };
 
 
+#ifdef CONFIG_ESP_MODEM_URC_HANDLER
+command_result handle_urc(uint8_t *data, size_t len)
+{
+    ESP_LOG_BUFFER_HEXDUMP("on_read", data, len, ESP_LOG_INFO);
+    return command_result::TIMEOUT;
+}
+#endif
+
 extern "C" void simple_cmux_client_main(void)
 {
     /* Init and register system/core components */
